9c4135440ba694409c7dfb07800d45fd0eff6df3
GamingSupreme <joshualeite2003@gmail.com>
2021-03-22 12:23:11 +0800

    Figured out reading files

--- Sorting an Array of Integers/Source.cpp
+++ Sorting an Array of Integers/Source.cpp
@@ -9,6 +9,29 @@ using namespace std;
 
 void main()
 {
+	ifstream readFile;
+	readFile.open("Test.txt");
+	int order;
+	readFile >> order;
+	int complexity;
+	readFile >> complexity;
+	int numberOfNumbers;
+	readFile >> numberOfNumbers;
+	vector<int> nums;
+	for (int i = 1; i <= numberOfNumbers; i++)
+	{
+		int temp;
+		readFile >> temp;
+		nums.push_back(temp);
+	}
+	cout << "order " << order << endl;
+	cout << "complexity " << complexity << endl;
+	cout << "Number of Numbers " << numberOfNumbers << endl;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		cout << nums[i] << " ";
+	}
+	cout << endl;
 
 	system("pause");
 }
